e8d898afe0b4a0f10bb1da55d829842544dcca05
branRoberts <brandonkrob12@gmail.com>
2026-06-09 14:38:22 -0400

    Second Commit Added two interfaces and added code to the startup runner class

--- src/main/java/com/pluralsight/sneakerdrops/models/Sneaker.java
+++ src/main/java/com/pluralsight/sneakerdrops/models/Sneaker.java
@@ -1,4 +1,57 @@
 package com.pluralsight.sneakerdrops.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Sneaker {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    String model;
+    double price;
+    int releaseYear;
+
+    public Sneaker(String model, double price, int releaseYear) {
+        this.model = model;
+        this.price = price;
+        this.releaseYear = releaseYear;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(int releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public Sneaker() {}
+
 }
--- src/main/java/com/pluralsight/sneakerdrops/startuprunner/StartupRunner.java
+++ src/main/java/com/pluralsight/sneakerdrops/startuprunner/StartupRunner.java
@@ -1,4 +1,37 @@
 package com.pluralsight.sneakerdrops.startuprunner;
 
-public class StartupRunner {
+import com.pluralsight.sneakerdrops.data.BrandRepository;
+import com.pluralsight.sneakerdrops.models.Brand;
+import com.pluralsight.sneakerdrops.service.DropService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StartupRunner implements CommandLineRunner {
+
+    private final BrandRepository brandRepository;
+    private DropService dropService;
+
+    @Autowired
+    public StartupRunner(DropService dropService, BrandRepository brandRepository) {
+        this.dropService = dropService;
+        this.brandRepository = brandRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println(dropService.getStatus());
+        seedData();
+        brandRepository.findAll().forEach(b -> System.out.println(b.getId() + " " + b.getName()));
+    }
+    private void seedData(){
+        if(brandRepository.count()==0){
+            brandRepository.save(new Brand("Nike"));
+            brandRepository.save(new Brand("Adidas"));
+            brandRepository.save(new Brand("New Balance"));
+            brandRepository.save(new Brand("Puma"));
+            brandRepository.save(new Brand("Reebok"));
+        }
+    }
 }
